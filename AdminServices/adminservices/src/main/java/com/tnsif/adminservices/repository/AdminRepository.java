da9b5ccaddcac2cf8397c4de322096d00599b7d7
DEEPAK3903 <ladhi5661@gmail.com>
2025-10-17 20:46:35 +0530

    07

--- AdminServices/adminservices/src/main/java/com/tnsif/adminservices/repository/AdminRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tnsif.adminservices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.adminservices.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
-}
